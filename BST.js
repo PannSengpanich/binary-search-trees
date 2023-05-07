4f687f2d0afc3a823cf7bce2786333b8f561c77e
Pann Sengpanich <panncraftyo@gmail.com>
2023-05-07 21:46:25 +0700

    update

--- BST.js
+++ BST.js
@@ -1,37 +1,55 @@
 import BSTNode from "./BSTNode.js";
 function BST(arr) {
-  let root = null;
+  arr = [...new Set(arr)].sort((a, b) => a - b);
+  let root = buildTree(arr, 0, arr.length - 1);
+  function buildTree(array = arr, start = 0, end = arr.length - 1) {
+    if (start > end) return null;
+    let mid = Math.floor((start + end) / 2);
+    let r = BSTNode(array[mid]);
 
-  function buildTree() {
-    arr = [...new Set(arr)].sort((a, b) => a - b);
-    for (let value of arr) {
-      if (root == null) {
-        root = BSTNode(value); //initialize root
-      } else {
-        let currentNode = root;
-        while (currentNode != null) {
-          if (value < currentNode.data) {
-            if (currentNode.left != null) {
-              currentNode = currentNode.left;
-            } else {
-              currentNode.left = BSTNode(value);
-              break;
-            }
-          } else if (value > currentNode.data) {
-            if (currentNode.right != null) {
-              currentNode = currentNode.right;
-            } else {
-              currentNode.right = BSTNode(value);
-              break;
-            }
-          }
-        }
+    r.left = buildTree(array, start, mid - 1);
+    r.right = buildTree(array, mid + 1, end);
+    return r;
+  }
+
+  const prettyPrint = (node = root, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
+  function inserting(node) {
+    let currentNode = root;
+    while (currentNode.left != null && currentNode.right != null) {
+      if (node.data < currentNode.data) {
+        currentNode = currentNode.left;
+      } else if (node.data > currentNode.data) {
+        currentNode = currentNode.right;
       }
     }
-    return root;
+    if (node.data < currentNode.data) {
+      currentNode.left = node;
+    } else if (node.data > currentNode.data) {
+      currentNode.right = node;
+    }
   }
-  root = buildTree();
-  return { buildTree };
+  function deleting(node) {
+    let currentNode = root;
+
+    return currentNode;
+  }
+  return { buildTree, root, prettyPrint, inserting, deleting };
 }
 let tree = BST([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-tree.buildTree();
+console.log(tree.prettyPrint());
+let a = BSTNode(10);
+let b = BSTNode(8.5);
+tree.insert(a);
+tree.insert(b);
+console.log(tree.prettyPrint());

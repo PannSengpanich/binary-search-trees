c670be45ff769375069d73902a639f85475ec155
Pann Sengpanich <panncraftyo@gmail.com>
2023-05-08 00:04:01 +0700

    Update BST.js

--- BST.js
+++ BST.js
@@ -43,7 +43,7 @@ function BST(arr) {
     root = deleteRec(root, num);
   }
   function deleteRec(root, num) {
-    if ((root = null)) return root;
+    if (root == null) return root;
     if (num < root.data) {
       root.left = deleteRec(root.left, num);
     } else if (num > root.data) {
@@ -65,14 +65,55 @@ function BST(arr) {
     }
     return minv;
   }
+  function find(num) {
+    let currentNode = root;
+    while (currentNode != null) {
+      if (num < currentNode.data) {
+        currentNode = currentNode.left;
+      } else if (num > currentNode.data) {
+        currentNode = currentNode.right;
+      } else if (num == currentNode.data) return currentNode;
+    }
+    return null;
+  }
+  function levelOrder(root, callback) {
+    if (!root) return [];
+    const queue = [root];
+    const result = [];
+    while (queue.length) {
+      const level = [];
+      const levelSize = queue.length;
+      for (let i = 0; i < levelSize; i++) {
+        const node = queue.shift();
+        level.push(node.data);
+        if (node.left) queue.push(node.left);
+        if (node.right) queue.push(node.right);
+        if (callback) callback(node); // invoke the callback function with the current node
+      }
+      result.push(level);
+    }
+    return result.flat();
+  }
 
-  return { buildTree, root, prettyPrint, inserting, deleting };
+  return {
+    root,
+    buildTree,
+    prettyPrint,
+    inserting,
+    deleting,
+    find,
+    levelOrder,
+  };
 }
 let tree = BST([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 console.log(tree.prettyPrint());
+console.log("-------------------------------------------------------");
 let a = BSTNode(10);
 let b = BSTNode(8.5);
 tree.inserting(a);
 tree.inserting(b);
 console.log(tree.prettyPrint());
-tree.deleting(1);
+tree.deleting(8);
+console.log(tree.prettyPrint());
+console.log(tree.find(4));
+console.log(tree.levelOrder(tree.root));
